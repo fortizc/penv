22e9ed695aff2ff7e465e5705b240076c30d0981
Felipe Ortiz <fortizc@gmail.com>
2017-10-12 23:00:48 -0300

    Use a env var to locate the config file

--- config.sh
+++ config.sh
@@ -1,45 +1,42 @@
-get_folder() {
-    local path=$HOME/.config/penv/config
-    if [ -f $path ]; then
-        local dir=$(cat $path |grep pyenv_folder |cut -d "=" -f2)
+get_value() {
+    if [ -f "$PENV_CONFIG_FILE" ]; then
+        local val=$(cat $PENV_CONFIG_FILE |grep $1 |cut -d "=" -f2)
     else
         echo "The config file doesn't exist"
         return 1
     fi
+    echo $val
+}
 
-    if [ -z $dir ]; then
+get_folder() {
+    local val=$(get_value "pyenv_folder")
+
+    if [ -z "$val" ]; then
         echo "No folder set, please set your folder in the config file"
         return 1
     fi
 
-    if [ ! -d "$dir" ]; then
+    if [ ! -d "$val" ]; then
         echo "The folder set doesn't exist"
         return 1
     fi
 
-    echo $dir
+    echo $val
 }
 
 get_python_path() {
-    local path=$HOME/.config/penv/config
+    local val=$(get_value "python_interpreter")
 
-    if [ -f $path ]; then
-        local dir=$(cat $path |grep python_interpreter |cut -d "=" -f2)
-    else
-        echo "The config file doesn't exist"
-        return 1
-    fi
-
-    if [ -z $dir ]; then
+    if [ -z "$val" ]; then
         echo "No path set, please set the path in the config file"
         return 1
     fi
 
-    if [ ! -d "$dir" ]; then
+    if [ ! -d "$val" ]; then
         echo "The path set doesn't exist"
         return 1
     fi
 
-    echo $dir
+    echo $val
 
 }
